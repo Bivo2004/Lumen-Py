49c6cf8fa326ecd598e4783849f72b1e1ee5ce5e
Bivo <mamdouhbevnoty@gmail.com>
2026-04-05 21:35:17 +0200

    chore: add setup.py for PyPI release

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="lumen-py",
+    version="1.0.0",
+    description="An AI-powered, local, Object-Oriented coding mentor.",
+    author="Bivo",
+    # We list the individual files that make up your tool
+    py_modules=["main", "deep_mapper", "llm_handler", "socratic_engine", "visualizer", "train_deep_model"],
+    # These are the libraries pip will automatically install for the user
+    install_requires=[
+        "torch",
+        "transformers",
+        "datasets",
+        "typer",
+        "rich",
+        "pyperclip",
+        "requests"
+    ],
+    # This creates the global terminal command "lumen"
+    entry_points={
+        "console_scripts": [
+            "lumen=main:app", 
+        ],
+    },
+)
